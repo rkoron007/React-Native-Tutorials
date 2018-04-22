06e7b756d941e1fb754b11017155d814eba38628
Rose <rkoron007@gmail.com>
2018-04-22 00:04:48 -0700

    Finished Basic Library App

---
+++ auth/src/components/LoginForm.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { Text } from "react-native";
+import { Button, Card, CardSection, Input, Spinner } from "./common";
+import firebase from "firebase";
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      error: "",
+      loading: false
+    };
+    this.renderButton = this.renderButton.bind(this);
+    this.onLoginSucess = this.onLoginSucess.bind(this);
+    this.onLoginFail = this.onLoginFail.bind(this);
+  }
+
+  renderButton(){
+    if (this.state.loading){
+      return <Spinner size="small" />;
+    } else {
+      return (
+        <Button onPress={this.onButtonPress.bind(this)}>
+          Login
+        </Button>
+      );
+    }
+  }
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    this.setState({ error: '', loading: true });
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSucess.bind(this))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(this.onLoginSucess.bind(this))
+          .catch(this.onLoginFail.bind(this));
+      });
+  }
+
+  onLoginSucess(){
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+  }
+
+  onLoginFail(){
+    this.setState({ error: 'WRONG', loading: false});
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            placeholder="password"
+            secureTextEntry
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          {this.renderButton()}
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: "center",
+    color: "red"
+  }
+};
+
+
+export default LoginForm;
---
+++ tech_stack/src/components/ListItem.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation } from "react-native";
+import { CardSection } from "./common";
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class ListItem extends React.Component{
+  renderDescription(){
+    const { library, expanded } = this.props;
+    if (expanded){
+      return (
+        <CardSection>
+          <Text style={{flex:1}}>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
+  componentWillUpdate(){
+    LayoutAnimation.spring();
+  }
+
+  render(){
+    const { titleStyle } = styles;
+    const { id, title } = this.props.library;
+
+    return(
+      <TouchableWithoutFeedback
+      onPress={() => this.props.selectLibrary(id)}
+      >
+      <View>
+        <CardSection>
+          <Text style={titleStyle}>
+            {title}
+          </Text>
+        </CardSection>
+        {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15,
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return {
+    expanded
+  };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
